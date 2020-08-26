349757bbfbef8c07aa6c3152ce56e8aa397c4071
Matheus Reis <matheus.reis@olist.com>
2020-08-26 14:25:59 -0300

    Added database settings

--- sellers/sellers/settings.py
+++ sellers/sellers/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'sellers.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'topskills5',
+        'USER': 'topskills5',
+        'PASSWORD': 'ote2020',
+        'HOST': 'pgsql03-farm70.uni5.net',
+        'PORT': ''                 # set to empty string for default
     }
 }
 
