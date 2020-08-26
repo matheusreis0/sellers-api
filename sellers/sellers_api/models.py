1811a7dba19811c95ff7074e52cf04182c3eb6a7
Matheus Reis <matheus.reis@olist.com>
2020-08-26 15:01:22 -0300

    Added sellers model and migrations. Added sellers_api to settings.py

--- sellers/sellers_api/models.py
+++ sellers/sellers_api/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+import uuid
+
 
 # Create your models here.
+class Seller(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    company_name = models.TextField(max_length=128)
+    brand = models.TextField(max_length=128)
+    status = models.TextField()
+    portfolio_size = models.IntegerField()
+    origin = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    blocked = models.BooleanField()
+    blocked_reason = models.TextField()
