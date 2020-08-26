da1d4198f30bae09353f080d107fac1cb5919b40
Matheus Reis <matheus.reis@olist.com>
2020-08-26 16:38:51 -0300

    sellers api done

--- sellers/sellers_api/views.py
+++ sellers/sellers_api/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Seller
+from .serializers import SellerSerializer
+
 
 # Create your views here.
+class SellerApi(viewsets.ModelViewSet):
+    queryset = Seller.objects.all().order_by('created_at')
+    serializer_class = SellerSerializer
